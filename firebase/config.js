7d0db6d2f2965f45afe92b50cfd136cb053e67f9
ypeng115@gmail.com <ypeng115@gmail.com>
2022-12-26 05:43:29 +0100

    Update firebase config keys

--- firebase/config.js
+++ firebase/config.js
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: 'AIzaSyAsg7vjdsS8rgOp0sugOxY0LqiGNTQcVgs',
+  authDomain: 'yum-book.firebaseapp.com',
+  projectId: 'yum-book',
+  storageBucket: 'yum-book.appspot.com',
+  messagingSenderId: '459996699090',
+  appId: '1:459996699090:web:0687cda538172bc053900b',
 };
 
 initializeApp(firebaseConfig);
