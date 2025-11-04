6ab70241b7b79ef691bb49ea36d502a7f5b034ed
ipeng5 <109869177+ipeng5@users.noreply.github.com>
2025-11-04 22:17:10 +0100

    Update footer link to valid URL

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -3,7 +3,7 @@ function Footer() {
     <footer className="text-center h-14 md:h-16 lg:h-24 text-xs md:text-sm lg:text-base w-full bg-gray-light flex items-center justify-center">
       &#169;&nbsp;{new Date().getFullYear()}&nbsp;&nbsp;Yum Book, built
       by:&nbsp;&nbsp;
-      <a href="https://www.ianpeng.dev" target="_blank" rel="noreferrer">
+      <a href="https://ianpeng.netlify.app" target="_blank" rel="noreferrer">
         <span className="text-primary-normal hover:underline">Ian Peng</span>
       </a>
     </footer>
